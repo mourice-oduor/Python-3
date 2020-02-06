fd44407e0d28de99540f5c41c59ac8b55a85d300
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 07:58:44 -0500

    calendar module to display specific year

---
+++ time_date_calendar modules/calendar_module.py
@@ -0,0 +1,10 @@
+import calendar
+'''calendar.isleap(2019)
+print(calendar.month(2020,2))
+print(calendar.month(2020,4))'''
+
+print(calendar.calendar(2020,2,1,10))
+''''where:(2,1,10) rep;
+2-maximum width btwn each character
+1-maximum lines per week
+10-spaces in between the months'''
