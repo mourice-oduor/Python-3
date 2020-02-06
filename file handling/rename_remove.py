d1e89402a97076379c67f2ec37f2efcacceb07b1
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 11:34:53 -0500

    renaming/removing files

---
+++ file handling/rename_remove.py
@@ -0,0 +1,5 @@
+import os
+os.rename('/home/net/MORYSO/PYTHON/class tutorials/Beginner/file handling/b.txt','/home/net/MORYSO/PYTHON/class tutorials/Beginner/file handling/bb.txt')
+
+#to remove file
+'''os.remove('/home/net/MORYSO/PYTHON/class tutorials/Beginner/file handling/bb.txt')'''
