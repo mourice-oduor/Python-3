04eb447da704efbcb242a68791bb1e3fb2703dfa
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 07:26:49 -0500

    dictionary methods

---
+++ data structures/dictionary.py
@@ -0,0 +1,11 @@
+#represented on curly braces
+#it has a key and a value pair
+
+age = {'john':24,'peter':25,'bob':27,'joy':65}
+
+age['williams'] = 55
+
+a = [1,2,3,4,5]
+b = [6,7,8,9]
+
+dict1 = {'a':[1,2,3,4,5],'b':[6,7,8,9]}
