860171345be1f31798c06f2914f32c934be252c2
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 11:28:51 -0500

    working with directories

---
+++ file handling/directories_folders.py
@@ -0,0 +1,16 @@
+#making directories
+import os
+os.mkdir("xxx")
+
+#to find where the directory is created use:
+os.getcwd()
+
+#to change directories:
+#os.chdir(path)
+#os.getcwd()
+
+#to remove a directory
+os.rmdir("xxx")
+os.getcwd()
+
+
