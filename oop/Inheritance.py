6b4d61d9827929e0598d7b0a38334c15e3f9593e
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 11:58:51 -0500

    class inheritance

---
+++ oop/Inheritance.py
@@ -0,0 +1,13 @@
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        print("The name is {} and the age is {}".format(name,age))
+
+class teacher(person):
+    def print(self,name,age):
+        person.__init__(self,name,age)
+
+class student(person):
+    def print(self,name,age):
+        person.__init__(self,name,age)
