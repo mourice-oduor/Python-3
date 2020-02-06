dab711c5f9c9f60da1055a4f3da4ae1d9932e5aa
mourice-oduor <otienomaurice364@gmail.com>
2020-02-05 21:48:46 -0500

    added a module demo

---
+++ functions/modules.py
@@ -0,0 +1,18 @@
+'''import math
+math.sqrt(16)'''
+
+def addition(n1,n2):
+    n3 = n1 + n2
+    print(n3)
+
+def subtraction(n1,n2):
+    n3 = n1 - n2
+    print(n3)
+
+def division(n1,n2):
+    n3 = n1 / n2
+    print(n3)
+
+def multiplication(n1,n2):
+    n3 = n1 * n2
+    print(n3)
