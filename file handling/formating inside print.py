64f9c6592163cf24935e598bdcafce9ae765e3fb
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 11:37:28 -0500

    print formating method

---
+++ file handling/formating inside print.py
@@ -0,0 +1,24 @@
+#normal print format
+name = 'moryso'
+age = 25
+per = 80.5
+print("The name is",name,"and the age is",age,"and the percentage is",per)
+
+#using string format
+name = 'moryso'
+age = 25
+per = 80.5
+print("The name is %s and the age is %s and the percentage is %s"%(name,age,per))
+
+#using float format
+name = 'moryso'
+age = 25
+per = 80.5
+print("The name is %s and the age is %f and the percentage is %f"%(name,age,per))
+print("The name is %s and the age is %.2f and the percentage is %.2f"%(name,age,per))
+
+#using integer format
+name = 'moryso'
+age = 25
+per = 80.5
+print("The name is %s and the age is %d and the percentage is %d"%(name,age,per))
