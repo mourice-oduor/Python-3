6bc8b3a96d77ced504e9189503e34ff3c75925d5
mourice-oduor <otienomaurice364@gmail.com>
2020-02-06 07:31:35 -0500

    alphabets using dictionary method

---
+++ data structures/dict2.py
@@ -0,0 +1,7 @@
+alphabet = {"A": "Apple", "B": "Boy", "C": "Cow", "D": 'Dog', "E": "Elephant", "F": "Fish", "G": "Ghost",
+"H": "Horse", "I": "Icecream", "J": "Jack", "K": "Kangaroo", "L": "Lamb", "M": "Moon", "N": "Nine", 
+"O": "Owl", "P": "Pig", "Q": "Queen", "R": "Ruler", "S": "Schoolbag", "T": "Train", "U": "Umbrella",
+"V": "Violet", "W": "Wind", "X": "Xylophone", "Y": "Yo-yo", "Z": "Zebra"}
+
+for i in alphabet:
+	print("{} for {}".format(i, alphabet[i]))
